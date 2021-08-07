5c011f4bb352da12b06a02dedc3a88429d71c63e
kurs <kurs@DESKTOP-V3HDKPQ>
2021-08-07 11:47:09 +0200

    update

---
+++ RentACar/FormLogin.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+using RentACar.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RentACar
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            String cs = ConfigurationManager.AppSettings["cs"];
+            try
+            {
+                if (
+                    String.IsNullOrWhiteSpace(tbLogin.Text) ||
+                    String.IsNullOrWhiteSpace(tbPassword.Text) 
+                    )
+                {
+                    DialogHelper.E("Podaj dane do logowania");
+                    return;
+                }
+
+                cs = String.Format(cs, tbLogin.Text.Trim(), tbPassword.Text.Trim());
+                new MySqlConnection(cs);
+            }
+            catch (Exception exc)
+            {
+
+            }
+        }
+    }
+}
