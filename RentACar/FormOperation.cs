831b6af4a77f40db940ea02796bcfd12f944547a
kurs <kurs@DESKTOP-V3HDKPQ>
2021-08-08 15:09:35 +0200

    update

--- RentACar/FormOperation.cs
+++ RentACar/FormOperation.cs
@@ -22,11 +22,18 @@ namespace RentACar
             InitializeComponent();
         }
 
+        int LastRecordId; 
         private void FormOperation_Load(object sender, EventArgs e)
         {
             if (OperBack)
             {
                 Text = "Zdaj auto -" + RegPlate;
+                DataTable dt = GlobalData.ExecSQL($"select id from operations where car_id={CarId} order by id desc limit 0,1 ");
+                if (dt.Rows.Count>0)
+                {
+                    LastRecordId = Convert.ToInt32(dt.Rows[0]["id"]);
+                }
+
             } else
             {
                 Text = "Wydaj auto -" + RegPlate;
@@ -39,7 +46,10 @@ namespace RentACar
             if (OperBack)
             {
                 // aktualizacja rekordu w operacjach
-                sql = "";
+                sql = @"update operations set
+                        ts_in=@ts, mileage_in=@mileage, description=concat(description,';',@descr)
+                        where id=@id
+                    ";
             } else
             {
                 // dodanie rekordu
@@ -55,6 +65,7 @@ namespace RentACar
             cmd.Parameters.AddWithValue("@ts", dtDate.Value);
             cmd.Parameters.AddWithValue("@mileage", numMileage.Value);
             cmd.Parameters.AddWithValue("@descr", tbDescr.Text);
+            cmd.Parameters.AddWithValue("@id", LastRecordId);
             cmd.ExecuteNonQuery();
 
             sql = "update cars set avail=@avail where id=@car_id";
--- RentACar/GlobalData.cs
+++ RentACar/GlobalData.cs
@@ -1,6 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +11,14 @@ namespace RentACar
     static class GlobalData
     {
         public static MySqlConnection connection = null;
+
+        public static DataTable ExecSQL(string sql)
+        {
+            DataTable dt = new DataTable();
+            MySqlDataAdapter da = new MySqlDataAdapter();
+            da.SelectCommand = new MySqlCommand(sql, connection);
+            da.Fill(dt);
+            return dt;
+        }
     }
 }
